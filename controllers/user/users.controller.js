28d4e8ddf48774bdc7a94f52d7140f4b023f5b48
Sayed Siam <msiam18@gmail.com>
2023-01-25 01:14:17 +0600

    Added User config

--- controllers/user/users.controller.js
+++ controllers/user/users.controller.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const userModel = require("")
 
 async function createUser(req,res){
     console.log(req.body);
+    console.log(User)
+    res.status(200).json({
+        "status" : 200,
+        "message" : "User Logged in successfully",
+        "data" : req.body,
+    });
 }
 
 
